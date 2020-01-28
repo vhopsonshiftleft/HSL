8d7f95487c02f3e58d93e7efa8189914b892a39e
vhopsonshiftleft <59857868+vhopsonshiftleft@users.noreply.github.com>
2020-01-28 13:58:35 -0500

    Update uat.sh

--- uat.sh
+++ uat.sh
@@ -1,5 +1,5 @@
-sl run -- java -jar target/hello-shiftleft-0.0.1.jar &
-sleep 60
+./sl run -- java -jar target/hello-shiftleft-0.0.1.jar &
+sleep 90
 curl http://localhost:8081/customers/1
 curl http://localhost:8081/customers/
 curl http://localhost:8081/patients
